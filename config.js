acb3a86e812b0a4db3c795b6794280abd2539a7b
Jeremy Condon <jeremy@grit-design.com>
2014-06-25 17:14:43 -0400

    Updated config to use https

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 		This is pretty much for internal (sizzlepig) development convenience
 */
 
-exports.SIZZLEPIG = 	process.env['SIZZLEPIG_URL']		|| "http://www.sizzlepig.com";
-exports.SIZZLEPIG_API = process.env['SIZZLEPIG_API_URL'] 	|| "http://api.sizzlepig.com";
+exports.SIZZLEPIG = 	process.env['SIZZLEPIG_URL']		|| "https://www.sizzlepig.com";
+exports.SIZZLEPIG_API = process.env['SIZZLEPIG_API_URL'] 	|| "https://api.sizzlepig.com";
 exports.PORT = process.env['SIZZLEPIG_SAMPLE_API_PORT'] || 3000;
